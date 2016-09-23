2cd0e77ce76610f41df3fb9d9c528c4b48bcfc19
jiajun <jjgithub@gmail.com>
2016-09-23 16:08:49 +0800

    Added image name to class mapping script

---
+++ script/ClassMappingScript.py
@@ -0,0 +1,37 @@
+import os
+import cPickle
+
+TRAINING_SET_FILEPATH = 'image/data'
+MAPPING_FILEPATH = 'ITCM.dat'
+
+if os.name == 'nt':
+    PATH_DELIMITER = '/'
+else:
+    PATH_DELIMITER = os.sep
+
+if __name__ == '__main__':
+    
+    mapping = {}
+    
+    print 'Initiate mapping process'
+    
+    for categoryName in os.listdir(TRAINING_SET_FILEPATH):
+        categoryPath = TRAINING_SET_FILEPATH + PATH_DELIMITER + categoryName
+        for imageName in os.listdir(categoryPath):
+            categoryLst = mapping.get(imageName)
+            if categoryLst is None:
+                categoryLst = []
+                mapping[imageName] = categoryLst
+            else:
+                print 'There is multiple category =', imageName
+            categoryLst.append(categoryName)
+    
+    #mapping = cPickle.load('ITCM.dat')
+    
+    #outputFile = open(MAPPING_FILEPATH, 'w')
+    #cPickle.dump(mapping, outputFile, protocol=cPickle.HIGHEST_PROTOCOL)
+    #outputFile.close()
+    
+    print 'Mapping process completed'
+    
+    print 'mapping =\n', mapping 
